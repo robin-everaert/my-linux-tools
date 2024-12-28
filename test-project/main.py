35177d405dff3005e02e9c5d796842e415ecdda4
Everaert Robin <r-everaert@outlook.fr>
2024-12-28 01:10:43 +0100

    App ready and deployed

--- test-project/main.py
+++ test-project/main.py
@@ -1,1 +1,13 @@
-print("Hello, Linux Tools!")
+#!/usr/bin/env python3
+import tkinter as tk
+
+# Create window
+root = tk.Tk()
+root.title("My Linux App")
+
+# add text
+label = tk.Label(root, text="Hello, Linux!")
+label.pack(pady=20, padx=20)
+
+# Launch app
+root.mainloop()
